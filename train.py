a7e9bf2f05ed473802911a66ff196402b5dd1df5
Erkosh04 <erkoja1104@gmail.com>
2025-11-23 22:42:34 +0500

    Update train.py

--- train.py
+++ train.py
@@ -3,27 +3,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle
+import os
 
-# Датасет жүктеу
-df = pd.read_csv("data/fake_labeled.csv")  # файл атауын өзіңдікіне ауыстыр
+df = pd.read_csv("data/fake_labeled.csv")
 
-# X және y
 X = df["text"]
 y = df["label"]
 
-# Text → TF-IDF
-vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
+vectorizer = TfidfVectorizer(stop_words="english", max_features=5000)
 X = vectorizer.fit_transform(X)
 
-# Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Model
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
-# Save model
+os.makedirs("model", exist_ok=True)
+
 with open("model/model.pkl", "wb") as f:
     pickle.dump((model, vectorizer), f)
-
-print("Модель сақталды: model/model.pkl")
